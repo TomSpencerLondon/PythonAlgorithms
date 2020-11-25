56db3d94012ab118046e8c8535283748dccf5715
Tom <thomas.spencer@codurance.com>
2020-11-25 13:52:14 +0000

    Add towersOfHanoi

--- recursion.py
+++ recursion.py
@@ -1,8 +1,40 @@
 # calculates factorial of a positive integer
+print("factorial")
+
 
 def factorial(n):
     if n == 0:
         return 1
     return n * factorial(n - 1)
 
-print(factorial(6))
+
+print(factorial(3))
+print("------")
+print("printFunc")
+
+
+# Recursion and Memory
+def printFunc(n):
+    if n == 0:
+        return 0
+    else:
+        print(n)
+        return printFunc(n - 1)
+    print(printFunc(n))
+
+
+printFunc(4)
+
+print("------")
+
+
+def towersOfHanoi(numberOfDisks, startPeg=1, endPeg=3):
+    if numberOfDisks:
+        towersOfHanoi(numberOfDisks - 1, startPeg, 6 - startPeg - endPeg)
+        print("Move disk %d from peg %d to peg %d" % (numberOfDisks, startPeg, endPeg))
+        towersOfHanoi(numberOfDisks - 1, 6 - startPeg - endPeg, endPeg)
+
+
+towersOfHanoi(numberOfDisks=4)
+
+print("------")
